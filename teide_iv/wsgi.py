a135e212a2a5148b191dbd52091c09b6308fa31c
sdeprado-TEI <sdeprado@teideformacion.com>
2026-06-28 12:33:43 +0200

    Update wsgi.py

--- teide_iv/wsgi.py
+++ teide_iv/wsgi.py
@@ -15,3 +15,23 @@ except Exception as e:
 application = get_wsgi_application()
 
 app = application
+# ... (lo que ya tienes de migrate) ...
+try:
+    django.setup()
+    call_command('migrate', '--noinput')
+
+    # Crear Administrador
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        admin = User.objects.create_superuser('admin', 'admin@teide.es', 'adminteideIV')
+        print("Usuario admin creado")
+
+    # Crear Profesor
+    if not User.objects.filter(username='profesor').exists():
+        profesor = User.objects.create_user('profesor', 'profesor@teide.es', 'profesorteideIV')
+        profesor.is_staff = True  # O si prefieres que tenga acceso limitado, puedes quitar esta línea
+        profesor.save()
+        print("Usuario profesor creado")
+
+except Exception as e:
+    print(f"Error: {e}")
