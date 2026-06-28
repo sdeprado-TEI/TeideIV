62534db0cfd1d72fe5440debca82948cb00b5511
sdeprado-TEI <sdeprado@teideformacion.com>
2026-06-28 13:45:14 +0200

    Update settings.py

--- teide_iv/settings.py
+++ teide_iv/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+"""
+Django settings for teide_iv project.
+...
+"""
+
+import os  # <--- ESTA ES LA LÍNEA QUE FALTA
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+...
 
 
 # Quick-start development settings - unsuitable for production
