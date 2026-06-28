f9ea387c7e1b9d6e2146729096bde5de96290f80
sdeprado-TEI <sdeprado@teideformacion.com>
2026-06-28 12:27:29 +0200

    Update wsgi.py

--- teide_iv/wsgi.py
+++ teide_iv/wsgi.py
@@ -1,19 +1,17 @@
-"""
-WSGI config for teide_iv project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teide_iv.settings')
 
-application = get_wsgi_application()
+# Intentamos ejecutar las migraciones automáticamente al arrancar
+try:
+    django.setup()
+    call_command('migrate', '--noinput')
+except Exception as e:
+    print(f"Error en migración automática: {e}")
 
+application = get_wsgi_application()
 
 app = application
